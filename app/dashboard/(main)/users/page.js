d36b75f84cc8335dabcaf513dea53653fe547d4e
Javad Esmaeili <cs.esmaeili@gmail.com>
2025-01-20 10:02:19 +0330

    fix(ui): Users page ui fixed

--- app/dashboard/(main)/users/page.js
+++ app/dashboard/(main)/users/page.js
@@ -1,34 +1,39 @@
 'use client'
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import NavigationMenu from '@/components/dashboard/NavigationMenu';
 import BlurLoading from '@/components/dashboard/BlurLoading';
 import CreateUser2 from '@/components/dashboard/users/CreateUser2';
 
 export default function user({ selfMode = false }) {
 
+
     const [page, setPage] = useState("createUser");
     const [loading, setLoading] = useState(false);
+    const scrollbarRef = useRef();
+
 
 
     return (
-        <div className=' flex relative grow gap-3 '>
+        <div className='flex flex-wrap p-5 flex-row-reverse xl:flex-nowrap relative grow gap-3 overflow-y-auto' ref={scrollbarRef}>
 
             <BlurLoading loading={loading} />
-            <div className='w-3/4'>
-                {(page == "createUser") && <CreateUser2 setParentLoading={setLoading} />}
-            </div>
 
-            <div className='w-1/4'>
+            <div className='w-full h-fit xl:w-1/4'>
                 <NavigationMenu
                     page={page} setPage={(page) => { setLoading(true); setPage(page); }}
-                    containerClass={"flex-col pl-3"}
+                    containerClass={"flex-col xl:pl-3"}
                     items={[
                         { page: "createUser", icon: "dashboard", label: "ساخت کاربر" },
                         { page: "userList", icon: "dashboard", label: "لیست کاربران" },
                     ]}
                 />
             </div>
+            <div className='w-full h-fit xl:w-3/4 overflow-hidden'>
+                {(page == "createUser") && <CreateUser2 setParentLoading={setLoading} scrollbarRef={scrollbarRef} />}
+            </div>
+
+
         </div >
     )
 }
--- components/dashboard/users/CreateUser2.jsx
+++ components/dashboard/users/CreateUser2.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import CustomInput from '../CustomInput';
 import { BsImage } from "react-icons/bs";
 import InputDatePicker from '../InputDatePicker';
@@ -6,86 +6,139 @@ import { BsCake2 } from "react-icons/bs";
 import Lottie from "lottie-react";
 import Teacher from '@/public/assets/animations/teacher.json';
 import Company from '@/public/assets/animations/company.json';
+import Degree from '@/public/assets/animations/degree.json';
 
 
-const CreateUser2 = ({ setParentLoading }) => {
+const CreateUser2 = ({ setParentLoading, scrollbarRef }) => {
 
     const [userType, setUserType] = useState('normal');
 
-    useEffect(() => {
 
+    useEffect(() => {
+        if (userType != "normal") {
+            scrollbarRef.current.scrollTop = scrollbarRef.current.scrollHeight;
+        }
+    }, [userType]);
+
+    useEffect(() => {
         setParentLoading(false);
     }, []);
 
-    return (
-        <div className='flex flex-col grow max-h-full overflow-x-auto gap-3 bg-primary rounded-xl p-5'>
 
-            <div className='flex flex-row grow gap-5'>
-                <div className='flex flex-col w-4/6 gap-3'>
+    return (
+        <div className='flex flex-col grow max-h-full overflow-hidden gap-3 bg-primary rounded-xl p-5'>
+
+            <div className='flex flex-col-reverse md:flex-row grow gap-5'>
+                <div className='flex flex-col w-full md:w-4/6 gap-3'>
                     <CustomInput rightLabel={"نام و نام خانوادگی (فارسی)"} inputClassName={"bg-secondary"} />
                     <CustomInput rightLabel={"کد ملی"} inputClassName={"bg-secondary"} />
                     <CustomInput rightLabel={"نام پدر"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
                     <InputDatePicker icon={<BsCake2 />} />
-
                 </div>
-                <div className='flex items-center justify-center bg-secondary w-2/6 p-20 rounded-md'>
+                <div className='flex items-center justify-center bg-secondary w-full md:w-2/6 p-20 rounded-md'>
                     <BsImage className='text-5xl rounded' />
                 </div>
             </div>
-            <div className='flex grow gap-5 justify-center items-end'>
-                <CustomInput rightLabel={"شماره شبا"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
-                <CustomInput rightLabel={"شماره کارت"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
-            </div>
-            <div className='flex grow gap-5'>
-                <CustomInput rightLabel={"استان"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
-                <CustomInput rightLabel={"شهر"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
-            </div>
-            <div className='flex grow gap-5'>
-                <CustomInput rightLabel={"آدرس"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
-            </div>
-            <div className='flex grow gap-5'>
-                <CustomInput rightLabel={"کد پستی"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
-            </div>
 
-            <div className='flex grow gap-3'>
-                <div className={`w-1/2 flex flex-col bg-secondary rounded-lg p-3 justify-center items-center border-2 border-transparent hover:border-accent
-                 ${userType == "company" && "!border-accent"}`}
-                    onClick={() => setUserType("company")}>
-                    <Lottie animationData={Company} className='w-1/2 h-60' />
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 grow">
+                <div
+                    className={`flex flex-col bg-secondary rounded-lg p-3 justify-center items-center border-2 border-transparent hover:border-accent ${userType == "company" && "!border-accent"}`}
+                    onClick={() => {
+                        if (userType == "company") {
+                            setUserType("normal");
+                            return;
+                        }
+                        setUserType("company");
+                    }}>
+                    <Lottie animationData={Company} className="w-1/2 h-60" />
                     <span>شرکت / حقوقی هستم</span>
                 </div>
-                <div className={`w-1/2 flex flex-col bg-secondary rounded-lg p-3 justify-center items-center border-2 border-transparent hover:border-accent
-                ${userType == "teacher" && "!border-accent"}`}
-                    onClick={() => setUserType("teacher")}>
-                    <Lottie animationData={Teacher} className='w-1/2 h-60' />
+                <div
+                    className={`flex flex-col bg-secondary rounded-lg p-3 justify-center items-center border-2 border-transparent hover:border-accent ${userType == "teacher" && "!border-accent"}`}
+                    onClick={() => {
+                        if (userType == "teacher") {
+                            setUserType("normal");
+                            return;
+                        }
+                        setUserType("teacher");
+                    }}>
+                    <Lottie animationData={Teacher} className="w-1/2 h-60" />
                     <span>مدرس هستم</span>
+                </div>
+                <div
+                    className={`sm:col-span-2 md:col-span-1 flex flex-col bg-secondary rounded-lg p-3 justify-center items-center border-2 border-transparent hover:border-accent ${userType == "degree" && "!border-accent"}`}
+                    onClick={() => {
+                        if (userType == "degree") {
+                            setUserType("normal");
+                            return;
+                        }
+                        setUserType("degree");
+                    }}
+                >
+                    <Lottie animationData={Degree} className="w-1/2 h-60" />
+                    <span>نیاز به مدرک دارم</span>
                 </div>
             </div>
 
+
+            {(userType == "degree") &&
+                <>
+                    <div className='flex flex-col md:flex-row grow gap-5 justify-center items-end'>
+                        <CustomInput rightLabel={"شماره شبا"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
+                        <CustomInput rightLabel={"شماره کارت"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
+                    </div>
+                    <div className='flex flex-col md:flex-row grow gap-5'>
+                        <CustomInput rightLabel={"استان"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
+                        <CustomInput rightLabel={"شهر"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
+                    </div>
+                    <div className='flex flex-col md:flex-row grow gap-5'>
+                        <CustomInput rightLabel={"آدرس"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
+                    </div>
+                    <div className='flex flex-col md:flex-row grow gap-5'>
+                        <CustomInput rightLabel={"کد پستی"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
+                    </div>
+
+                </>
+            }
             {(userType == "teacher") &&
                 <>
-                    <div className='flex grow gap-5'>
-                        <CustomInput rightLabel={"نام شرکت"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
-                        <CustomInput rightLabel={"کد  ثبت شرکت"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
+                    <div className='flex flex-col md:flex-row grow gap-5 justify-center items-end'>
+                        <CustomInput rightLabel={"شماره شبا"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
+                        <CustomInput rightLabel={"شماره کارت"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
                     </div>
-                    <div className='flex grow gap-5'>
-                        <CustomInput rightLabel={"کد اختصاصی شرکت"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
+                    <div className='flex flex-col md:flex-row grow gap-5'>
+                        <CustomInput rightLabel={"استان"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
+                        <CustomInput rightLabel={"شهر"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
                     </div>
+                    <div className='flex flex-col md:flex-row grow gap-5'>
+                        <CustomInput rightLabel={"آدرس"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
+                    </div>
+                    <div className='flex flex-col md:flex-row grow gap-5'>
+                        <CustomInput rightLabel={"کد پستی"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
+                    </div>
+                    <div className='flex flex-col md:flex-row grow gap-5'>
+                        <CustomInput rightLabel={"درباره من"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
+                        <CustomInput rightLabel={"گیت هاب"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
+                    </div>
+                    <div className='flex flex-col md:flex-row grow gap-5'>
+                        <CustomInput rightLabel={"لینکدین"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
+                        <CustomInput rightLabel={"تلگرام"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
+                    </div>
+                    <div className='flex flex-col md:flex-row grow gap-5'>
+                        <CustomInput rightLabel={"اینستا"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
+                        <CustomInput rightLabel={"توییتر"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
+                    </div>
+
                 </>
             }
             {(userType == "company") &&
                 <>
-                    <div className='flex grow gap-5'>
-                        <CustomInput rightLabel={"درباره من"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
-                        <CustomInput rightLabel={"گیت هاب"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
+                    <div className='flex flex-col md:flex-row grow gap-5'>
+                        <CustomInput rightLabel={"نام شرکت"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
+                        <CustomInput rightLabel={"کد  ثبت شرکت"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
                     </div>
-                    <div className='flex grow gap-5'>
-                        <CustomInput rightLabel={"لینکدین"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
-                        <CustomInput rightLabel={"تلگرام"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
-                    </div>
-                    <div className='flex grow gap-5'>
-                        <CustomInput rightLabel={"اینستا"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
-                        <CustomInput rightLabel={"توییتر"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
+                    <div className='flex flex-col md:flex-row grow gap-5'>
+                        <CustomInput rightLabel={"کد اختصاصی شرکت"} inputClassName={"bg-secondary"} containerClassName={"w-full"} />
                     </div>
                 </>
             }
