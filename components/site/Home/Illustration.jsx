8fc32650b3f5a71d0ffdd59b6a70ff5dff54bbd1
Javad Esmaeili <cs.esmaeili@gmail.com>
2025-03-11 12:53:26 +0330

    style(ui): Core Colors changed

--- components/site/Home/Illustration.jsx
+++ components/site/Home/Illustration.jsx
@@ -6,8 +6,10 @@ const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
 const Illustration = () => {
     return (
-        <div className="relative w-full  h-45 sm:h-50 md:h-56 lg:h-70  lg:h-[27rem] order-1 lg:order-none">
-            <Lottie animationData={Think} className="w-full h-full" />
+        <div className="flex grow items-center justify-center">
+            <div className="relative w-full  h-45 sm:h-50 md:h-56 lg:h-70  lg:h-[27rem] order-1 lg:order-none">
+                <Lottie animationData={Think} className="w-full h-full" />
+            </div>
         </div>
     );
 };
